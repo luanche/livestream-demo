1e7b2f1f1aa5fc5d26928a76a22d55904ba426e3
luanche <1054129686@qq.com>
2024-10-26 23:08:35 +0800

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-docker run --rm -d -p 1935:1935 -p 1985:1985 -p 8080:8080 \
+docker run --rm -d -p 1935:1935 -p 1985:1985 -p 8080:8080 --name srs \
+    -v ./index.html:/usr/local/srs/objs/nginx/html/players/player.html \
     registry.cn-hangzhou.aliyuncs.com/ossrs/srs:5
